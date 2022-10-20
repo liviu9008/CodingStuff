d7eb1701214f69b137f8e3eae0c9751285d977ac
liviu9008 <celcan.liviu@yahoo.com>
2022-10-20 08:20:59 +0300

    second version

---
+++ file_removal_after_x_days.py
@@ -0,0 +1,15 @@
+import os
+import time, datetime
+
+path = r"C:/Users/liviu/OneDrive/Desktop/Python Projects/Coding stuff/results/"
+now = time.time()
+days_until_removal = 7
+
+for filename in os.listdir(path):
+    if os.path.isfile(os.path.join(path, filename)): #file check
+        creation_time = os.path.getctime(os.path.join(path, filename))
+        UTC_time = datetime.datetime.fromtimestamp(creation_time, tz=datetime.timezone.utc).strftime('%Y-%m-%d %H:%M:%S')
+        print(UTC_time)
+        if os.path.getctime(os.path.join(path, filename)) < now - days_until_removal * 86400: #file removal after x days
+            print(filename)
+            # os.remove(os.path.join(path, filename))
